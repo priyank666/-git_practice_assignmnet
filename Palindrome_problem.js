2781536226a1441b6317968f74633577c3d90ef1
priyank666 <priyankgautam670@gmail.com>
2022-10-23 11:21:12 +0530

    Prime Number And Palindrome Problem

---
+++ Palindrome_problem.js
@@ -0,0 +1,13 @@
+    var str = naman;
+    var N = 5;
+    var new_str="";
+    
+    for (var i=N-1; i>=0; i--){
+        new_str = new_str + str[i];
+    }
+    if(str==new_str){
+        console.log("Yes");
+    }
+    else {
+        console.log("No");
+    }
---
+++ Prime_Problem.js
@@ -0,0 +1,14 @@
+ let num = 13;
+ let c = 0;
+
+ for (i = 1; i <= num; i++){
+    if(num % i ==0) {
+        
+    }
+ }
+ if(c = 2) {
+    console.log("Yes");
+ }
+ else {
+    console.log("No");
+ }
