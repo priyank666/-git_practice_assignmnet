071a5880009daf13a8e7b9861ab9c7e07c562253
priyank666 <priyankgautam670@gmail.com>
2022-11-05 23:25:37 +0530

    code fixed for prime

--- prime.js
+++ prime.js
@@ -1,19 +1,20 @@
-function checkPrime(num){
+function checkPrime(number){
     let factors = 0;
-    for(let i=1; i<=num; i++){
-        if(num % i){
+
+    for(let i=1; i<=number; i++){
+        if(number % i == 0){
             factors++;
         }
     }
-    if(factors) {
+    if(factors == 2) {
         return true;
     }
     return false;
 }
 
-let ans = checkPrime(13);
+let answer = checkPrime(13);
 
-if(ans ==  true){
+if(answer ==  true){
     console.log("Prime");
 } else {
     console.log("Not Prime");
